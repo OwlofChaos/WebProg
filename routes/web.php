b53edd761b43aea6adce468f107f7e7f571fa23a
OwlofChaos <Christiannathaniel45@gmail.com>
2024-12-23 01:26:41 +0700

    bisa

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,50 @@
 <?php
 
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\DonateController;
+// use App\Http\Controllers\ProjectController;
+
+// Route::get('/donate', [DonateController::class, 'index'])->name('donate.index');
+// Route::post('/donate', [DonateController::class, 'submit'])->name('donate.submit');
+
+// Route::get('/aboutus', [DonateController::class, 'aboutus'])->name('aboutus');
+// Route::get('/impact', [DonateController::class, 'impact'])->name('impact');
+// Route::get('/home', [DonateController::class, 'home'])->name('home');
+// // Route::get('/login', [DonateController::class, 'login'])->name('login');
+// // Route::get('/register', [DonateController::class, 'register'])->name('register');
+// Route::get('/project', [ProjectController::class, 'project'])->name('project');
+
+// Route::get('/', function () {
+//     if (Auth::check()) {
+//         return redirect()->route('home');
+//     }
+//     return redirect()->route('login.form');
+// });
+
+// use App\Http\Controllers\AuthController;
+
+// Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
+// Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+// Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+// Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DonateController;
+use App\Http\Controllers\ProjectController;
 
 Route::get('/donate', [DonateController::class, 'index'])->name('donate.index');
 Route::post('/donate', [DonateController::class, 'submit'])->name('donate.submit');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/aboutus', [DonateController::class, 'aboutus'])->name('aboutus');
+Route::get('/impact', [DonateController::class, 'impact'])->name('impact');
+Route::get('/home', [DonateController::class, 'home'])->name('home');
+Route::get('/login', [DonateController::class, 'login'])->name('login');
+Route::get('/register', [DonateController::class, 'register'])->name('register');
+Route::get('/project', [ProjectController::class, 'project'])->name('project');
+
+Route::get('/', function () {
+    return redirect()->route('home');
+});
