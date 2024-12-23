5abe0a5683083641bd1b94db5fddefd55a64737f
Owl of Chaos <114159412+OwlofChaos@users.noreply.github.com>
2024-12-23 19:26:46 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
@@ -99,7 +99,7 @@
         <img src="/images/240_F_772366252_6FMVyELqVBAOofqo1dAnIlZWqttiAB7l.jpg" alt="Sea Turtle">
     </div>
     <div class="form-container">
-        <h2>REGISTRATION</h2>
+        <h2>LOGIN</h2>
         <form method="POST" action="{{ route('login') }}">
             @csrf
             <input type="email" class="form-control" id="email" name="email" placeholder="Email" required value="{{ old('email') }}">
